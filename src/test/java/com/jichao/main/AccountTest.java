23082a1cf98263393717cd296aef5837b53d5970
Jichao <jicahoo@163.com>
2016-11-04 14:30:56 +0800

    Create

--- src/test/java/com/jichao/main/AccountTest.java
+++ src/test/java/com/jichao/main/AccountTest.java
@@ -4,10 +4,14 @@ import com.jichao.entity.Account;
 import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 import org.junit.Test;
 
 import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 /**
  * Created by zhangj52 on 11/4/2016.
@@ -15,22 +19,60 @@ import java.util.List;
 public class AccountTest {
 
     @Test
-    public void testHibernate() {
+    public void createAndQuery() {
         SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
-        Session session = sessionFactory.openSession();
-        Account account = new Account();
-        account.setName("First");
-        account.setId("account_1");
-        session.persist(account);
-        session.flush();
-        Query query = session.createQuery("from Account");
-        List accounts = query.list();
-        System.err.println(accounts.size());
-        for (Object obj : accounts) {
-            Account oneAccount = (Account)obj;
-            System.err.println(oneAccount.getId());
-            System.err.println(oneAccount.getName());
+        Session session = null;
+        Transaction tx = null;
+        try {
+            session = sessionFactory.openSession();
+            tx = session.beginTransaction();
+            Account account = new Account();
+            account.setName("First");
+            account.setId("account_1");
+            session.persist(account);
+            session.flush();
+            Query query = session.createQuery("from Account");
+            List accounts = query.list();
+            System.err.println(accounts.size());
+            for (Object obj : accounts) {
+                Account oneAccount = (Account) obj;
+                assertEquals("First", account.getName());
+                assertEquals("account_1", account.getId());
+            }
+            //tx.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+            tx.rollback();
+        }finally {
+            session.close();
+            tx.rollback();
         }
-        session.close();
     }
+
+    @Test
+    public void queryId() {
+        SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
+        Session session = null;
+        Transaction tx = null;
+        try {
+            session = sessionFactory.openSession();
+            tx = session.beginTransaction();
+            Account account = new Account();
+            account.setName("First");
+            account.setId("account_2");
+            session.persist(account);
+            session.flush();
+            Query query = session.createQuery("select account.id from Account account");
+            List accounts = query.list();
+            System.err.println(accounts.size());
+            assertTrue(accounts.contains("account_2"));
+        } catch (Exception e) {
+            e.printStackTrace();
+            tx.rollback();
+        } finally {
+            session.close();
+            tx.rollback();
+        }
+    }
+
 }
