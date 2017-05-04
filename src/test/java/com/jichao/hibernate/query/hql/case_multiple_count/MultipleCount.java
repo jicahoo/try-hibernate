d01c57226c3cbd7613acc6c4e10083165efbbcc0
jicahoo <jicahoo@163.com>
2017-05-04 13:50:04 +0800

    MultipleCount can work

--- src/test/java/com/jichao/hibernate/query/hql/case_multiple_count/MultipleCount.java
+++ src/test/java/com/jichao/hibernate/query/hql/case_multiple_count/MultipleCount.java
@@ -12,7 +12,9 @@ import org.junit.Test;
 
 import java.io.File;
 import java.util.List;
+import java.util.Map;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
 
 /**
@@ -56,23 +58,41 @@ public class MultipleCount {
             //       "left join sr.pools pools_1 group by sr.id order by id";
             //String hql = "select count(*) from StorageResourceBean sr";
 
-            String hql = "select host.id, host.name from com.jichao.hibernate.query.hql.case_multiple_count.model.Host host";
+            String hql = "SELECT host.id AS host_id, host.name AS host_name, count(DISTINCT iscsi.id) AS iscsi_count, " +
+                    "count(DISTINCT fc.id) AS fc_count FROM com.jichao.hibernate.query.hql.case_multiple_count.model.Host host " +
+                    "LEFT OUTER JOIN host.iscsiHostInitiators AS iscsi " +
+                    "LEFT OUTER JOIN host.fcHostInitiators fc " +
+                    "GROUP BY host.id,host.name";
 
             Query query = session.createQuery(hql);
             query.setResultTransformer(Transformers.ALIAS_TO_ENTITY_MAP);
 
             List objs = query.list();
             for (Object obj : objs) {
-                System.out.println(obj.getClass());
-            }
+                Map<String, Object> propMap = (Map<String, Object>)obj;
+                String objId = (String)propMap.get("host_id");
+                Object fcCount = propMap.get("fc_count");
+                Object iscsiCount = propMap.get("iscsi_count");
 
-            System.out.println("Hello " + objs);
+                if (objId.equals("host_1")) {
+                    assertEquals(1L, fcCount);
+                    assertEquals(1L, iscsiCount);
+
+                }
+                if (objId.equals("host_2")) {
+                    assertEquals(3L, fcCount);
+                    assertEquals(1L, iscsiCount);
+                }
+
+            }
 
         } catch (Exception e) {
             e.printStackTrace();
             fail("Failed to query.");
         } finally {
-            session.close();
+            if (session != null) {
+                session.close();
+            }
         }
 
     }
--- src/test/java/com/jichao/hibernate/query/hql/case_multiple_count/sql/create_table.sql
+++ src/test/java/com/jichao/hibernate/query/hql/case_multiple_count/sql/create_table.sql
@@ -1,19 +1,33 @@
 -- !!MUST set autocommit, or the data won't be persisted.
 SET AUTOCOMMIT TRUE;
-CREATE  TABLE test_jichao (name varchar(20));
+CREATE TABLE test_jichao (name varchar(50));
 
-CREATE TABLE host (id varchar(20), name varchar(20));
+CREATE TABLE host (id varchar(50), name varchar(50));
 
-CREATE TABLE host_initiator (id varchar(20), name varchar(20));
+CREATE TABLE host_initiator (id varchar(50), name varchar(50));
 
-CREATE TABLE host_iscsiHostInitiators (id varchar(20), elementvalue varchar(20));
+CREATE TABLE host_iscsiHostInitiators (id varchar(50), elementvalue varchar(50));
 
-CREATE TABLE host_fcHostInitiators (id varchar(20), elementvalue varchar(20));
+CREATE TABLE host_fcHostInitiators (id varchar(50), elementvalue varchar(50));
 
 INSERT INTO host(id, name) VALUES ('host_1', 'host_1_name'), ('host_2','host_2_name');
 
-INSERT INTO host_initiator (id, name) VALUES ('initiator_1', 'initiator_1_name'), ('initiator_2', 'initiator_2_name');
+INSERT INTO host_initiator (id, name) VALUES
+    ('initiator_1', 'iscsi_initiator_1_name'),
+    ('initiator_2', 'iscsi_initiator_2_name'),
+    ('initiator_3', 'fc_initiator_3_name'),
+    ('initiator_4', 'fc_initiator_4_name'),
+    ('initiator_5', 'fc_initiator_5_name'),
+    ('initiator_6', 'iscsi_initiator_6_name');
 
-INSERT INTO host_iscsiHostInitiators (id, elementvalue) VALUES ('host_1', 'initiator_1'), ('host_2', 'initiator_2');
-INSERT INTO host_fcHostInitiators (id, elementvalue) VALUES ('host_2', 'initiator_2');
+
+INSERT INTO host_iscsiHostInitiators (id, elementvalue) VALUES
+    ('host_1', 'initiator_1'),
+    ('host_2', 'initiator_2');
+INSERT INTO host_fcHostInitiators (id, elementvalue) VALUES
+    ('host_1', 'initiator_6'),
+    ('host_2', 'initiator_3'),
+    ('host_2', 'initiator_4'),
+    ('host_2', 'initiator_5');
+
 SELECT * FROM host;
